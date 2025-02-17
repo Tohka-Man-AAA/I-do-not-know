f3d66adf38c8207a57dcfb66f9da82bb6622f911
Tohka-Man-AAA <antoshechkasiu@gmail.com>
2025-02-17 17:56:56 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,13 +1,45 @@
-memes = {'КРИНЖ': 'Что-то очень странное или стыдное',
-        'ЛОЛ':"Что-то очень смешное",
-        "РОФЛ":"Шутка",
-        'КРИПОВЫЙ' : 'Страшный, пугающий',
-        'АГРИТЬСЯ' : 'злиться'}
-print('Приветствую вас в словаре молодежного лексикона!')
-print('У вас есть возможность узнать значение 5 слов, удачи!')
-for i in range(5):
-    word = input('Введите непонятное слово:')
-    if word.upper() in memes.keys():
-        print(memes[word.upper()])
-    else:
-        print('Такого слова в словаре пока нет')
+#Импорт
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+def result_calculate(size, lights, device):
+    #Переменные для энергозатратности приборов
+    home_coef = 100
+    light_coef = 0.04
+    devices_coef = 5   
+    return size * home_coef + lights * light_coef + device * devices_coef 
+
+#Первая страница
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+#Вторая страница
+@app.route('/<size>')
+def lights(size):
+    return render_template(
+                            'lights.html', 
+                            size=size
+                           )
+
+#Третья страница
+@app.route('/<size>/<lights>')
+def electronics(size, lights):
+    return render_template(
+                            'electronics.html',
+                            size = size, 
+                            lights = lights                           
+                           )
+
+#Расчет
+@app.route('/<size>/<lights>/<device>')
+def end(size, lights, device):
+    return render_template('end.html', 
+                            result=result_calculate(int(size),
+                                                    int(lights), 
+                                                    int(device)
+                                                    )
+                        )
+app.run(debug=True)
